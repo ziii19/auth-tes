8a1dc593b360432a7f436db82e017aee8f61f761
ziiii08 <fauzi08.dev@gmail.com>
2025-01-13 20:30:45 +0700

    feat: add custom exceptions handling

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,9 @@
+class ServerException implements Exception {
+  final String message;
+  ServerException(this.message);
+}
+
+class AuthenticationException implements Exception {
+  final String message;
+  AuthenticationException(this.message);
+}
