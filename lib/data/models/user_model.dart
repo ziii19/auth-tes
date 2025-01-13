338b91179ce60127f4fbbd9fb4ef4a29902083dd
ziiii08 <fauzi08.dev@gmail.com>
2025-01-13 21:49:07 +0700

    feat: add user model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,32 @@
+import '../../domain/entities/user.dart';
+
+class UserModel extends User {
+  final String accessToken;
+  final String refreshToken;
+
+  UserModel({
+    required super.id,
+    required super.username,
+    required super.email,
+    required super.firstName,
+    required super.lastName,
+    required super.gender,
+    required super.image,
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      gender: json['gender'],
+      image: json['image'],
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+    );
+  }
+}
