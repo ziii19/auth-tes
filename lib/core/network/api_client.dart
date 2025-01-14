1bbad79974c99cc66df324cbbc0e43b7a2fe98be
ziiii08 <fauzi08.dev@gmail.com>
2025-01-14 10:24:47 +0700

    feat: add error handling

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -9,19 +10,43 @@ class ApiClient {
   Future<http.Response> post(String endpoint,
       {Map<String, String>? headers, Object? body}) async {
     final url = Uri.parse('$baseUrl$endpoint');
-    return await http.post(
-      url,
-      headers: headers ?? {'Content-Type': 'application/json'},
-      body: jsonEncode(body),
-    );
+    try {
+      final response = await http.post(
+        url,
+        headers: headers ?? {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      ).timeout(const Duration(seconds: 15), onTimeout: () {
+        throw TimeoutException('The connection has timed out');
+      });
+
+      if (response.statusCode < 200 || response.statusCode >= 300) {
+        throw Exception('Request failed with status: ${response.statusCode}');
+      }
+
+      return response;
+    } catch (e) {
+      throw Exception('Failed to make the request: $e');
+    }
   }
 
   Future<http.Response> get(String endpoint,
       {Map<String, String>? headers}) async {
     final url = Uri.parse('$baseUrl$endpoint');
-    return await http.get(
-      url,
-      headers: headers ?? {'Content-Type': 'application/json'},
-    );
+    try {
+      final response = await http.get(
+        url,
+        headers: headers ?? {'Content-Type': 'application/json'},
+      ).timeout(const Duration(seconds: 15), onTimeout: () {
+        throw TimeoutException('The connection has timed out');
+      });
+
+      if (response.statusCode < 200 || response.statusCode >= 300) {
+        throw Exception('Request failed with status: ${response.statusCode}');
+      }
+
+      return response;
+    } catch (e) {
+      throw Exception('Failed to make the request: $e');
+    }
   }
 }
--- lib/data/sources/remote/auth_remote_data_source.dart
+++ lib/data/sources/remote/auth_remote_data_source.dart
@@ -17,43 +17,57 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
 
   AuthRemoteDataSourceImpl({required this.apiClient, required this.prefs});
 
-   @override
+  @override
   Future<UserModel> login(String username, String password) async {
-    final response = await apiClient.post('/auth/login', body: {
-      'username': username,
-      'password': password,
-    });
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      prefs.setString('access_token', data['accessToken']);
-      prefs.setString('refresh_token', data['refreshToken']);
-      return UserModel.fromJson(data);
-    } else if (response.statusCode == 401) {
-      throw AuthenticationException('Invalid username or password');
-    } else {
-      throw ServerException('Failed to login');
+    try {
+      final response = await apiClient.post('/auth/login', body: {
+        'username': username,
+        'password': password,
+      });
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        await prefs.setString('access_token', data['accessToken']);
+        await prefs.setString('refresh_token', data['refreshToken']);
+
+        return UserModel.fromJson(data);
+      } else if (response.statusCode == 401) {
+        throw AuthenticationException('Invalid username or password');
+      } else {
+        throw ServerException('Failed to login');
+      }
+    } catch (e) {
+      throw ServerException(e.toString());
     }
   }
 
   @override
   Future<UserDetailModel> getUserDetail() async {
-    final accessToken = prefs.getString('access_token') ?? '';
+    try {
+      final accessToken = prefs.getString('access_token') ?? '';
 
-    if (accessToken.isEmpty) {
-      throw AuthenticationException('User not logged in');
-    }
+      if (accessToken.isEmpty) {
+        throw AuthenticationException('User not logged in');
+      }
 
-    final response = await apiClient.get('/user/me', headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer $accessToken',
-    });
+      final response = await apiClient.get(
+        '/user/me',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $accessToken',
+        },
+      );
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      return UserDetailModel.fromJson(data);
-    } else {
-      throw ServerException('Failed to fetch user details');
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return UserDetailModel.fromJson(data);
+      } else {
+        throw ServerException('Failed to fetch user details');
+      }
+    } on Exception catch (e) {
+      throw ServerException(e.toString());
     }
   }
 }
